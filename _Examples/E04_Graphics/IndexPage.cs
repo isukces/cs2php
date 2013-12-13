519ff93a7f0d80b61cdc95f2bea55b05c1da81f6
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-13 19:20:44 +0100

    Graph example added

---
+++ _Examples/E04_Graphics/IndexPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lang.Php;
+using Lang.Php.Graph;
+
+namespace E04_Graphics
+{
+    [Page("index")]
+    public class IndexPage : PhpDummy
+    {
+        public static void PhpMain()
+        {
+            //var a = GetImageSize(@"e:\Media\zdjecia\2013\236 24 sie\130824_140502.JPG");
+            //echo("Width=" + a.Width + ", height=" + a.Height);
+            //if (a.ImageType == Lang.Php.Graph.ImageTypes.Jpeg)
+            //    echo("Format = JPEG");
+            //var_dump(a);
+
+            header("Content-Type: image/png");
+            var myImage = Image.Create(200, 100);
+            if (Image.IsValid(myImage))
+            {
+                Color color = myImage.ColorAllocate(0, 0, 0); // first call is background
+                color = myImage.ColorAllocate(255, 0, 0);
+                myImage.DrawString(Font.Font2, 10, 10, "Hello", color);
+                myImage.Png();
+                //myImage.Jpeg();
+                //myImage.Jpeg(null);
+                //myImage.Jpeg(null, 40);
+                //myImage.Destroy();
+            }
+        }
+    }
+}
---
+++ _Examples/E04_Graphics/compile.bat
@@ -0,0 +1,6 @@
+@echo off
+set CS2PHP=..\..\Lang.Cs2Php\bin\Release\cs2php.exe
+set PROJ=E04_Graphics
+%CS2PHP% %PROJ%.csproj  ..\..\_ExamplesOut\%PROJ%
+
+
