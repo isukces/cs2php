f5c00520e7a0a36c6511506d9307e89293d1f9c3
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-13 19:19:50 +0100

    Introduced specialized translator for Graph related classes

---
+++ Lang.Php.Compiler/Translator/Node/GraphTranslator.cs
@@ -0,0 +1,33 @@
+﻿using Lang.Cs.Compiler;
+using Lang.Php.Compiler.Source;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lang.Php.Compiler.Translator.Node
+{
+    public class GraphTranslator : IPhpNodeTranslator<ClassFieldAccessExpression>
+    {
+        public IPhpValue TranslateToPhp(IExternalTranslationContext ctx, ClassFieldAccessExpression src)
+        {
+            if (src.Member.DeclaringType == typeof(Lang.Php.Graph.Font))
+            {
+                var name = src.Member.Name;
+                if (name =="Font1" || name =="Font2" || name =="Font3" || name =="Font4" || name =="Font5" ) {
+                    var size = int.Parse(name.Substring(4));
+                    return new PhpConstValue(size);
+                }
+                throw new NotImplementedException();
+            }
+            return null;
+        }
+
+        public int getPriority()
+        {
+            return 1;
+        }
+    }
+}
