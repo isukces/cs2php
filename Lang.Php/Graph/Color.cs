ce491ad24a52a5a56620fd13ed6b4a894a669877
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-14 15:26:23 +0100

    Error checking method for Color class

--- Lang.Php/Graph/Color.cs
+++ Lang.Php/Graph/Color.cs
@@ -8,5 +8,10 @@ namespace Lang.Php.Graph
     [Skip]
     public class Color
     {
+        [UseBinaryExpression("!==", "false", "$0")]
+        public static bool IsValid(Color image)
+        {
+            return image != null;
+        }
     }
 }
