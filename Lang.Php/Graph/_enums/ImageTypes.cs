d74560fb1bbe23ca0d4c28bf43d2575ff82f6531
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-14 11:31:52 +0100

    using PHP constants instead of numbers when translating ImageTypes.

--- Lang.Php/Graph/_enums/ImageTypes.cs
+++ Lang.Php/Graph/_enums/ImageTypes.cs
@@ -10,39 +10,39 @@ namespace Lang.Php.Graph
     {
         [RenderValue("0")]
         Unknown = 0,
-        [RenderValue("1")]
+        [RenderValue("IMG_GIF")]
         Gif = 1,
-        [RenderValue("2")]
+        [RenderValue("IMG_JPG")]
         Jpeg = 2,
-        [RenderValue("3")]
+        [RenderValue("IMAGETYPE_PNG")]
         Png = 3,
-        [RenderValue("4")]
+        [RenderValue("IMAGETYPE_SWF")]
         Swf = 4,
-        [RenderValue("5")]
+        [RenderValue("IMAGETYPE_PSD")]
         Psd = 5,
-        [RenderValue("6")]
+        [RenderValue("IMAGETYPE_BMP")]
         Bmp = 6,
-        [RenderValue("7")]
+        [RenderValue("IMAGETYPE_TIFF_II")]
         Tiff_Ii = 7,
-        [RenderValue("8")]
+        [RenderValue("IMAGETYPE_TIFF_MM")]
         Tiff_Mm = 8,
-        [RenderValue("9")]
+        [RenderValue("IMAGETYPE_JPC")]
         Jpc = 9,
-        [RenderValue("10")]
+        [RenderValue("IMAGETYPE_JP2")]
         Jp2 = 10,
-        [RenderValue("11")]
+        [RenderValue("IMAGETYPE_JPX")]
         Jpx = 11,
-        [RenderValue("12")]
+        [RenderValue("IMAGETYPE_JB2")]
         Jb2 = 12,
-        [RenderValue("13")]
+        [RenderValue("IMAGETYPE_SWC")]
         Swc = 13,
-        [RenderValue("14")]
+        [RenderValue("IMAGETYPE_IFF")]
         Iff = 14,
-        [RenderValue("15")]
+        [RenderValue("IMAGETYPE_WBMP")]
         Wbmp = 15,
-        [RenderValue("16")]
+        [RenderValue("IMAGETYPE_XBM")]
         Xbm = 16,
-        [RenderValue("17")]
+        [RenderValue("IMAGETYPE_ICO")]
         Ico = 17,    
     }
 }
