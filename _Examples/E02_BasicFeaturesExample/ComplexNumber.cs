6818f54f10ef38ff3af1b8e8cd7782e333fbbf82
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-19 14:19:30 +0100

    operators usage example included

---
+++ _Examples/E02_BasicFeaturesExample/ComplexNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lang.Php.Examples.BasicFeaturesExample
+{
+    [Module("class.complex-number")]
+    public class ComplexNumber
+    {
+        #region Constructors
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+
+        #endregion Constructors
+
+        #region Static Methods
+
+        // Public Methods 
+
+        [ScriptName("minus")]
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+
+        [ScriptName("plus")]
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+
+        public static implicit operator ComplexNumber(double x)
+        {
+            return new ComplexNumber(x, 0);
+        }
+
+        #endregion Static Methods
+
+        #region Properties
+
+        public double Imaginary { get; set; }
+
+        public double Real { get; set; }
+
+        #endregion Properties
+
+        [ScriptName("mul1")]
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+        }
+        [ScriptName("mul2")]
+        public static ComplexNumber operator *(ComplexNumber a, double b)
+        {
+            return new ComplexNumber(a.Real * b, a.Imaginary * b);
+        }
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            var tmp = b.Real * b.Real + b.Imaginary * b.Imaginary;
+            var re = a.Real * b.Real + a.Imaginary * b.Imaginary;
+            var im = a.Imaginary * b.Real - a.Real * b.Imaginary;
+            return new ComplexNumber(re / tmp, im / tmp);
+        }
+    }
+}
---
+++ _Examples/E02_BasicFeaturesExample/ComplexNumberTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lang.Php.Examples.BasicFeaturesExample
+{
+    [Page("complex-number-test")]
+    public class ComplexNumberTest
+    {
+        public static void PhpMain()
+        {
+            var a = new ComplexNumber(3, 2);
+            ComplexNumber b = 12;
+            var c = a * b;
+            var d = a + b;
+
+            var e = (a + 17) * 3;
+            var m1 = new ComplexNumber(3, 2) * new ComplexNumber(4, 5);
+            var m2 = new ComplexNumber(3, 2) * 5;
+
+        }
+    }
+}
