28953fe558efb5bb1dcaa05f5a83f2ab1c26de07
Piotr Stęclik <MrCompiler@cs2php.com>
2013-12-23 11:52:28 +0100

    switch translation example

---
+++ _Examples/E02_BasicFeaturesExample/SwichCaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lang.Php.Examples.BasicFeaturesExample
+{
+    [Page("swich-case")]
+    class SwichCaseTest : PhpDummy
+    {
+        public static void PhpMain()
+        {
+            int a = 2 * 3;
+            switch (a)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    echo("Something wrong");
+                    break;
+                case 6:
+                    echo("Everything is OK.");
+                    break;
+                default:
+                    echo("Something really wrong");
+                    break;
+
+            }
+        }
+    }
+}
