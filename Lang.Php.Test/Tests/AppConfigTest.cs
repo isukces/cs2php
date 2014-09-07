d78751ec8d6f73e5406d8de3450be8d295c64194
Piotr Stęclik <MrCompiler@cs2php.com>
2014-09-07 12:55:11 +0200

    append missing file

---
+++ Lang.Php.Test/Tests/AppConfigTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lang.Php.Compiler;
+using Xunit;
+
+namespace Lang.Php.Test.Tests
+{
+    class AppConfigTest
+    {
+        [Fact]
+        public static void Basic()
+        {
+            using (var x = new AppConfigManipulator())
+            {
+                
+            }
+        }
+    }
+}
