a8d34ed3446833858e94de5d4502b6cf1cdfda3b
Piotr Stęclik <MrCompiler@cs2php.com>
2014-11-22 16:44:45 +0100

    Class in subfolder/nested namespace included

---
+++ _Examples/E02_BasicFeaturesExample/SomeFolder/ClassInSubfolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lang.Php.Examples.BasicFeaturesExample.SomeFolder
+{
+
+    [Module("SomeFolder/ClassInSubfolder")]
+    public class ClassInSubfolder
+    {
+        public static void Hello()
+        {
+            PhpDummy.echo("ClassInSubfolder hello");
+        }
+    }
+}
